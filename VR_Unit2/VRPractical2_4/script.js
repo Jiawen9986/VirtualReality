d69886b030149ff2367b4fd14767e53f6b45eab8
Jiawen9986 <jiawenw6@nycstudents.net>
2025-10-30 00:32:14 +0000

    Refactor code structure for improved readability and maintainability

--- VR_Unit2/VRPractical2_4/script.js
+++ VR_Unit2/VRPractical2_4/script.js
@@ -3,12 +3,19 @@ let scene;
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); 
-
+  for(let i = 0; i < 100; i++){
+    let x = rnd(0,20);
+    let y = rnd(0,20);
+    let z = rnd(-20,20);
+    let rocket = new Rocket(x, y , z);
+  }
 
   loop();
 })
 
 function loop(){
+  rocket.setAttribute("rotation", {x:rocket.getAttribute("position").x+=0.1, y:0, z:0} );
+  rocket.launch();
 
   
   window.requestAnimationFrame( loop );
