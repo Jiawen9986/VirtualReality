286543995c4c63799ac1c6f9c78ae6d8a2e5acb4
Jiawen9986 <jiawenw6@nycstudents.net>
2025-11-14 15:54:10 +0000

    Refactor code structure for improved readability and maintainability

--- VR_Unit2/VRPractical2_5/script.js
+++ VR_Unit2/VRPractical2_5/script.js
@@ -11,18 +11,18 @@
 */
 
 let maze = [
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
+  "xxxxxxxxxxxxxxxxxxxxxx",
+  "-------xpppppppppx-xpx",
+  "xxxxxx-xpppppppppx-xpx",
+  "xppppx-xxxxxxxxxxx-xxx",
+  "xxxxxx-----x---------x",
+  "x----xxxxx-x-xxxxxxxxx",
+  "xxxx-xpppx-x-xpppppppx",
+  "xppx-xxxxx-x-xxxxxxxxx",
+  "xxxx-------x---x-----x",
+  "x-xx-x-xxx-xxx-x-xxx-x",
+  "x----x-xpx-------xpx-x",
+  "xxxxxxxxxxxxxxxxxxxx-x",
 ];
 
 /* Challenge 2
@@ -34,13 +34,17 @@ let scene;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   for(let r = 0; r < maze.length; r++){
-    /* Challenge 3
-      Choose a technique to traverse the each character in the string.
-    */ 
-    /* Challenge 4
-       Make an appropriate decision based on the characters you chose to enter 
-       in the maze.  Create an instance of the corresponding object.
-    */
+    let row = maze[r];
+    let cols = row.split("");
+    for(let c = 0; c < cols.length; c++){
+      if(cols[c] == "x"){
+        new Wall(c,1,r)
+      }
+      else if(cols[c] == "P"){
+        new Plant(c,1,r);
+    }
+
   }
+}
 
 })
