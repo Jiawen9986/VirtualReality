efe915c063a4e160d903be65b613a5d0dab67020
Jiawen9986 <jiawenw6@nycstudents.net>
2026-01-22 15:43:23 +0000

    Add Plant class to create and position plant objects in the scene

--- Escaping Project/maze.js
+++ Escaping Project/maze.js
@@ -1,18 +1,40 @@
 let maze = [
-  "xxxxxxxxxxxxxxxxxxxxxx",
-  "-------xpppppppppx-xpx",
-  "xxxxxx-xpppppppppx-xpx",
-  "xppppx-xxxxxxxxxxx-xxx",
-  "xxxxxx-----x---------x",
-  "x----xxxxx-x-xxxxxxxxx",
-  "xxxx-xpppx-x-xpppppppx",
-  "xppx-xxxxx-x-xxxxxxxxx",
-  "xxxx-------x---x-----x",
-  "x-xx-x-xxx-xxx-x-xxx-x",
-  "x----x-xpx-------xpx-x",
-  "xxxxxxxxxxxxxxxxxxxx-x",
+  "x-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "x-------------x-x-x-------x---x-----x",
+  "x-xpxxxxxxxxx-x-x-x-xxxxx-x-x-x-x-x-x",
+  "x-x-------x-x-x-x-------x---x-x-x-x-x",
+  "x-xxxxx-x-x-x-x-xxxxxxxxxxxxx-x-x-x-x",
+  "x-----x-x-x-x-x---x-------------x-x-x",
+  "xxxxx-x-x-x-x-xxx-x-xxxxx-xxxxxxx-x-x",
+  "x-----x-x-x---x---x-x---x-x-x-----x-x",
+  "x-xxxxxxx-xxxxx-xxx-x-x-x-x-x-xxxxx-x",
+  "x---x-x-------x-x---x-x-x-x-x-x-----x",
+  "xxx-x-x-xxxxx-x-x-xxx-x-x-x-x-x-xxxxx",
+  "x-p-x-x-x---x-x-x-x-x-x-x-x---x-----x",
+  "x-x-x---x-x-x-x-x-x---x-x-xxxxxxxxx-x",
+  "x-x-xxxxx-x-x-x-x-xxxxx-x-x---x---x-x",
+  "x-x-------x-x---x-x-----x-x---x-xxx-x",
+  "x-xxxxxxxxx-xxxxx-x-xxxxx-x-x-x-----x",
+  "x-x---x---x-------x---x---x-x-xxxxx-x",
+  "x-x-x-xxx-xxxxxxxxxxx-x-xxx-x-x---x-x",
+  "x---x-----x---x-----x-x-x---x-x-x-x-x",
+  "xxxxxxxxxxx-x-x-xxx-x-x-xxxxx-x-x-xxx",
+  "x---x-------x-----x---x-x-----x-x---x",
+  "x-x-x-xxxxxxxxx-xxxxxxx-x-xxxxx-xxx-x",
+  "x-x-x-x-------x-x-------x---x---x---x",
+  "x-x---x-xxxxx-xxx-xxxxxxxxx-x-xxx-xxx",
+  "x-xxxxx-x---x-x---x---x-----x-x---x-x",
+  "x-----x-x-x-x-x-xxx-x-x-x-xxx-x-xxx-x",
+  "xxxxx-x-x-x-x---x---x---x-x---x-----x",
+  "x---x-x-x-xxxxxxx-x-xxxxxxx-x-xxxxx-x",
+  "x-x-x-x-x---------x-x-------x-----x-x",
+  "x-x---x-xxxpxxxxxxxxx-xxxxxxxxx-xxx-x",
+  "x-x-xxx-----------x---x---x-----x---x",
+  "x-x-x---xpxxxxxxx-x-xxx-x-x-xxxxx-xxx",
+  "x-xxx-x-x-------x-x-x---x-x-x-x---x-x",
+  "x-----x-x-x---x-----x-x-x-----x-x---x",
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-x",
 ];
-
 let scene;
 
 window.addEventListener("DOMContentLoaded",function() {
@@ -26,8 +48,7 @@ window.addEventListener("DOMContentLoaded",function() {
       }
       else if(cols[c] == "p"){
         new Plant(c,1,r);
-    }
-
+      }
   }
 }
 
---
+++ Escaping Project/plant.js
@@ -0,0 +1,14 @@
+class Plant {
+  constructor(x,y,z){
+    this.x = x;
+    this.y = y;
+    this.z = y;
+    
+    this.obj = document.createElement("a-box");
+    this.obj.setAttribute("src","Plant.png");
+    this.obj.setAttribute("position",{x:x,y:y,z:z});
+    this.obj.setAttribute("height","2");
+    scene.append(this.obj);
+
+  }
+}
